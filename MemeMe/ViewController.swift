eea3a8067cd055cea8adb17385b202441e8322ce
bluermind <bluermind@gmail.com>
2016-12-22 13:09:23 +0900

    Add image picker function

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -20,6 +20,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func selectImage(_ sender: Any) {
+        let controller = UIImagePickerController()
+        present(controller, animated: true, completion: nil)
+    }
 
 }
 
